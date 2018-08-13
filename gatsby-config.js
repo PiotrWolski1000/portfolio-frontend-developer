33236999e06c5f9b4f126bdde280445be9b79946
piotr Wolski <p.wolski1000@gmail.com>
2018-08-13 14:23:41 +0200

    adding contentful config data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Piotr Wolski Frontend Developer',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'd0v97bwihzbp',
+        accessToken: '182b0d1015e9708603f95a5af715b69733eac2fa67956ba6405860a62b87c58a',
+      }
+    },
+    'gatsby-plugin-react-helmet',
+
+  ],
 }
