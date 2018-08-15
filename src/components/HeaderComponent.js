e5c4326b84a68c12121b0ab94c5e80c7ebed6150
piotr Wolski <p.wolski1000@gmail.com>
2018-08-15 23:53:37 +0200

    adding responsive menu added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,44 +1,117 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import $ from "jquery";
 
 const Wrapper = styled.div`
-background-color: #042740;
-width: 100%;
-height: 75px;
-border-bottom: #416B89 solid 2px;
-position: fixed;
-top: 0px;
+    background-color: #042740;
+    width: 100%;
+    height: 75px;
+    border-bottom: #416B89 solid 2px;
+    position: fixed;
+    top: 0px;
 
-display: flex;
-align-items: center;
-justify-content: flex-end;
-padding-right: 50px;
-z-index: 1;
-
-
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 25px;
+    z-index: 1;
 `
 const MainNav = styled.nav`
-    /* color: white; */
     a {
         color: white;
         text-decoration: none;
+        padding-left: 20px;
     }
+
+    ${Burger} svg {
+        display: none;
+    }
+
+    @media (max-width: 350px) {
+        display: flex;
+        /* flex-direction: column; */
+        align-content: center;
+        align-items: flex-end;
+
+        ${Burger} {
+            svg {
+                display: flex;
+                /* align-items: center; */
+                justify-content: center;
+                width: 50px;
+                height: 150px;
+                cursor: pointer;
+                -webkit-transform: translate3d(0, 0, 0);
+                -moz-transform: translate3d(0, 0, 0);
+                -o-transform: translate3d(0, 0, 0);
+                -ms-transform: translate3d(0, 0, 0);
+                transform: translate3d(0, 0, 0);
+            }
+            path {
+                fill: none;
+                -webkit-transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+                -moz-transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+                -o-transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+                -ms-transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+                transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+                stroke-width: 40px;
+                stroke-linecap: round;
+                stroke: #fff6e6;
+                stroke-dashoffset: 0px;
+            }
+            path#top,
+            path#bottom {
+                stroke-dasharray: 240px 950px;
+            }
+            path#middle {
+                stroke-dasharray: 240px 240px;
+            }
+            .cross path#top,
+            .cross path#bottom {
+                stroke-dashoffset: -650px;
+                stroke-dashoffset: -650px;
+            }
+            .cross path#middle {
+                stroke-dashoffset: -115px;
+                stroke-dasharray: 1px 220px;
+            }
+        }
+
+        a {
+            display: none;
+        }
+    }
+
 `
 
+const Burger = styled.div`
 
-
+`
 
 const HeaderComponent = ({}) => {
+
     return(
         <Wrapper>
             
-            {/* </PiotrWolski> */}
             <MainNav>
-                <a href="#about">About me</a> |
-                <a href="#contact">Contact</a> |
-                <a href="#skills">My skills</a> |
+                <Burger onClick={()=> {
+                          return $("div").toggleClass("cross");
+                }}>
+                    <div>
+                    <svg viewBox="0 0 800 600">
+                        <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
+                        <path d="M300,320 L540,320" id="middle"></path>
+                        <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+                    </svg>
+                    </div>
+                </Burger>
+                {/* <ul> */}
+                <a href="#about">About</a> 
+                <a href="#contact">Contact</a> 
+                <a href="#skills">Skills</a> 
                 <a href="#projects">Projects</a>
+                {/* </ul> */}
             </MainNav>
         </Wrapper>   
     )
