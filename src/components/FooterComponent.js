179a8b0bdbefe311b74957f7bacef8505a6a2233
piotr Wolski <p.wolski1000@gmail.com>
2018-09-03 00:36:57 +0200

    adding position relative

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -3,11 +3,14 @@ import styled from 'styled-components'
 
 
 const Wrapper = styled.div`
-background-color: #011727;
-color: white;
-width: 100%;
-height: 100px;
-border-top: #416B89 solid 2px;
+    background-color: #011727;
+    color: white;
+    width: 100%;
+    height: 100px;
+    border-top: #416B89 solid 2px;
+
+    position: relative;
+    top: 75px;
 `
 
 const FooterComponent = ({heroCardData}) => {
