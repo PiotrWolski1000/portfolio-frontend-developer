4be0663e326b768d20eeb98a868704827544ed87
piotr Wolski <p.wolski1000@gmail.com>
2018-09-03 00:36:24 +0200

    add inputs, textarea for email message detais, and css

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,23 +3,64 @@ import styled from 'styled-components'
 
 
 const Wrapper = styled.div`
-// background-color: red;
-width: 100%;
-height: 400px;
+    width: 100%;
+    height: 600px;
+
+    position: relative;/*because of fixed header*/
+    top: 75px;
 `
 
 const AddintionalWrapper = styled.div`
-    width: 95%;
     margin: 0 auto;
     height: 100%;
-    border-top: #416B89 solid 2px;
     clear: both;
+`
+const SectionTitle = styled.h1`
+    padding-top: 50px;
+    padding-bottom: 25px;
+    text-align: center;
+    width: 100%;
+    border-top: #416B89 solid 2px;
+    width : 95%;
+    margin: 0 auto;
+`
+const ContactForm = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    input, textarea {
+        display: block;
+        width: 80%;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        padding: 10px 0px;
+
+        border-radius: 10px;
+    }
+    #message {
+        
+    }
 `
 const ContactComponent = ({}) => {
     return(
         <Wrapper>
+            <SectionTitle>Contact me</SectionTitle>
             <AddintionalWrapper>
-                ContactComponent
+
+                <ContactForm>
+                    <input name="name" type='text' placeholder="Name, Company name"/>
+                    <input name="email" type="email" placeholder="Email address"/>
+                    <input name="phone" type="number" placeholder="Phone number"/>
+                    <input name="mail_subject" type="text" placeholder="Subject/Title"/>
+                    {/* <input /> */}
+                    <textarea name="message" type="text" rows="4" cols="50" placeholder="Message"> 
+                    {/* asd */}
+                    </textarea>
+
+                </ContactForm>
+
             </AddintionalWrapper>
         </Wrapper>   
     )
