3c1971508d110b7480747cd5ca206423299e5f35
piotr Wolski <p.wolski1000@gmail.com>
2019-02-03 15:09:56 +0100

    changing wrapper to section, instead of div element

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 
-const Wrapper = styled.div`
+const Wrapper = styled.section`
     width: 100%;
     height: auto;
     position: relative;
