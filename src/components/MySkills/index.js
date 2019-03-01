29f1f97ca868add486eaf889cda31a8d550c8f41
piotr Wolski <p.wolski1000@gmail.com>
2019-03-01 16:53:06 +0100

    adding new data items

--- src/components/MySkills/index.js
+++ src/components/MySkills/index.js
@@ -10,44 +10,68 @@ class index extends Component {
     render() {
         const data = [
             {
-                link: this.props.source.javascriptimage.url,
+                link: this.props.source.javascriptimage,
                 description: this.props.text[8].node.text
             },
             {
-                link: this.props.source.html5.url,
+                link: this.props.source.html5,
                 description: this.props.text[7].node.text                
             },
             {
-                link: this.props.source.css3.url,
+                link: this.props.source.css3,
                 description: this.props.text[6].node.text                
             },
             {
-                link: this.props.source.nodejs.url,
+                link: this.props.source.nodejs,
                 description: this.props.text[5].node.text                                                
             },
             {
-                link: this.props.source.react.url,
+                link: this.props.source.react,
                 description: this.props.text[4].node.text                                                
             },
             {
-                link: this.props.source.npm.url,
+                link: this.props.source.npm,
                 description: this.props.text[3].node.text                                                
             },
             {
-                link: this.props.source.photoshop.url,
+                link: this.props.source.photoshop,
                 description: this.props.text[2].node.text
             },
             {
-                link: this.props.source.ubuntuos.url,
+                link: this.props.source.ubuntuos,
                 description: this.props.text[1].node.text                                                
             },
             {
-                link: this.props.source.windowsos.url,
+                link: this.props.source.windowsos,
                 description: this.props.text[0].node.text                                                
             },
             {
-                link: this.props.source.github.url,
+                link: this.props.source.github,
                 description: this.props.text[9].node.text                                                
+            },
+            {
+                link: this.props.source.gatsbyjs,
+                description: this.props.text[10].node.text                                                
+            },
+            {
+                link: this.props.source.adobexd,
+                description: this.props.text[11].node.text                                                
+            },
+            {
+                link: this.props.source.netlify,
+                description: this.props.text[12].node.text                                                
+            },
+            {
+                link: this.props.source.styledComponents,
+                description: this.props.text[13].node.text                                                
+            },
+            {
+                link: this.props.source.contentful,
+                description: this.props.text[14].node.text                                                
+            },
+            {
+                link: this.props.source.datocms,
+                description: this.props.text[15].node.text                                                
             }
         ]
      
@@ -56,12 +80,17 @@ class index extends Component {
                 <h1 style={{textAlign: 'center'}}>
                     {this.props.sectionTitle}
                 </h1>
-                <Tag.AddintionalWrapper>
+
+               <Tag.AddintionalWrapper>
                     {data.map((item, i) => {
                         return(
                             <div className='flip3D' key={`description_card_flip3d_${i}`}>
                                 <div className='front' key={`description_card_div_front_${i}`}>
-                                    <img style={{width: '100px', height: '100px'}} src={item.link} key={`skill_img_${i}`}/>
+                                    {item.link.format === 'svg'?(
+                                        <img style={{width: '100px', height: '100px'}} src={item.link.url} key={`skill_img_${i}`}/>                                        
+                                    ):(
+                                        <Img fixed = {item.link.fixed}/> /* alt={item.link.alt} */
+                                    )}
                                 </div>
                                 <div key={`description_card_div_back_${i}`} className='back'>
                                     <p key={`description_card_${i}`}> {item.description} </p>
